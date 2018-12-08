4682d53afc640b9866c204e6b38decca0df3a410
SridharPavithrapu <srpa3332@colorado.edu>
2018-12-07 19:00:37 -0700

    Update TestBankAccount.java

--- TestBankAccount.java
+++ TestBankAccount.java
@@ -9,8 +9,8 @@ public class TestBankAccount {
 	@Test
 	public void testDebitWithSufficientFunds(){
 			BankAccount account = new BankAccount(10);
-			double amount = account.debit(5);
-			Assert.assertEquals(5.0, amount);
+			double validAmount = account.debit(5);
+			Assert.assertEquals(5.0, validAmount);
 	}
 	
 }
