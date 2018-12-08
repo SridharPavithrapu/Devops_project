ccae554b627bc2508ef96365b1904eb7d49ef2cd
SridharPavithrapu <srpa3332@colorado.edu>
2018-12-07 23:32:33 -0700

    Updated pom file

--- src/test/java/TransferContextTest.java
+++ src/test/java/TransferContextTest.java
@@ -5,6 +5,15 @@ import static org.junit.Assert.*;
 
 public class TransferContextTest {
 
-    
+    @Test
+    public void testTransfer() {
+        BankAccount a = new BankAccount("abc123", 15000);
+        BankAccount b = new BankAccount("def456", 25000);
+
+        new TransferContext(a, b).transfer(5000);
+
+        assertEquals((Integer)10000, a.getBalance());
+        assertEquals((Integer)30000, b.getBalance());
+    }
 
 }
